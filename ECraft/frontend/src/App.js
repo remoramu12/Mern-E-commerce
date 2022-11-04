1f5d3589b3747727328d9df0b8235cc62f3982d1
Ramkrishna Karmakar <remoramu12@Gmail.com>
2022-11-04 18:39:55 +0530

    create-react-app

---
+++ ECraft/frontend/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div >
+      <header >
+      <a href = "/"> ECraft</a>
+      </header>
+      <main>
+        List Items
+      </main>
+    </div>
+  );
+}
+
+export default App;
